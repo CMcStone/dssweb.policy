4b811022d96ad648d81f1dabf1f704a4ef81fda9
Carol McMasters-Stone <cbeck@ucdavis.edu>
2018-11-27 16:02:31 -0800

    getting an error but it appears to be a plone error.

--- dssweb/policy/sharing.py
+++ dssweb/policy/sharing.py
@@ -1,5 +1,7 @@
-from zope.interface import implements
-from plone.app.workflow.interfaces import ISharingPageRole
+from zope.interface import implements, Interface
+from Products.CMFPlone import PloneMessageFactory as _
+from plone.app.workflow.interfaces import ISharingPageRole as interfaceToImplement
+
 
 from Products.CMFPlone import PloneMessageFactory as _
 
@@ -10,7 +12,7 @@ Adding a contributor role to a special manage portlets role in the sharing tab
 
 
 class ManagePortletsRole(object):
-    implements(ISharingPageRole)
+    implements(interfaceToImplement)
 
     title = _(u"title_can_portlets", default=u"Manage Portlets")
     required_permission = "Sharing page: Manage Portlets"
